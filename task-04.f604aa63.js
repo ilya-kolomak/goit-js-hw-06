ab29483e9a88551b6be722f8875a9a29f3876bdc
ilya-kolomak <99127402+ilya-kolomak@users.noreply.github.com>
2022-09-08 17:37:00 +0000

    Deploying to gh-pages from @ ilya-kolomak/goit-js-hw-06@7eebd37347fd6eb39cbde0e9024c1e3c98876cfb 🚀

---
+++ task-04.f604aa63.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("#value");t.textContent=Number(0);var e=document.querySelector('button[data-action="decrement"]'),n=document.querySelector('button[data-action="increment"]');e.addEventListener("click",(function(){t.textContent=t.textContent-=1})),n.addEventListener("click",(function(){t.textContent=1+t.textContent++}))}();
+//# sourceMappingURL=task-04.f604aa63.js.map
