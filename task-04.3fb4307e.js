0f8498f4ca98cccd9cef9b029a5f94569266f081
ilya-kolomak <99127402+ilya-kolomak@users.noreply.github.com>
2022-09-09 13:21:54 +0000

    Deploying to gh-pages from @ ilya-kolomak/goit-js-hw-06@97ccd92d465276b7f0cb5f2a4784cca7a7dc1a2a 🚀

--- task-04.3fb4307e.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("#value");t.textContent=Number(0);const e=document.querySelector('button[data-action="decrement"]'),n=document.querySelector('button[data-action="increment"]');e.addEventListener("click",(()=>{t.textContent=t.textContent-=1})),n.addEventListener("click",(()=>{t.textContent=1+t.textContent++}));
-//# sourceMappingURL=task-04.3fb4307e.js.map
